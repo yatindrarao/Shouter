a43ca422a3250bf0061586afc63598afc785d306
Yatindra <yatindrarao.rao@gmail.com>
2017-03-17 10:09:09 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'homes#show',  via: :get
+  resource :dashboard, only: :show
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+end
